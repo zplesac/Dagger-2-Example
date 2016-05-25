cc34d53829e43d11a2410d2c85411f18dbea242a
zplesac <zeljko.plesac@infinum.hr>
2016-05-25 21:03:04 +0200

    Defined base database actions listener

---
+++ app/src/main/java/co/infinum/pokemon/database/DatabaseActionListener.java
@@ -0,0 +1,11 @@
+package co.infinum.pokemon.database;
+
+/**
+ * Created by Željko Plesac on 25/05/16.
+ */
+public interface DatabaseActionListener {
+
+    void onSuccess();
+
+    void onError(Exception e);
+}
