e16fc81c7f2abb187f437cadbabfbb8a89d67662
zplesac <zeljko.plesac@infinum.hr>
2016-05-25 20:56:38 +0200

    implementing actions

--- app/src/main/java/co/infinum/pokemon/database/DBFlowPokemonDAO.java
+++ app/src/main/java/co/infinum/pokemon/database/DBFlowPokemonDAO.java
@@ -1,10 +1,18 @@
 package co.infinum.pokemon.database;
 
+import com.raizlabs.android.dbflow.sql.language.CursorResult;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+import com.raizlabs.android.dbflow.structure.database.transaction.QueryTransaction;
+
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import co.infinum.pokemon.database.interfaces.DatabaseModelListener;
 import co.infinum.pokemon.database.interfaces.PokemonDAO;
 import co.infinum.pokemon.models.Pokemon;
+import timber.log.Timber;
 
 /**
  * Created by Željko Plesac on 25/05/16.
@@ -47,17 +55,35 @@ public class DBFlowPokemonDAO implements PokemonDAO {
     }
 
     @Override
-    public Pokemon getByName(String name, DatabaseModelListener<Pokemon> listener) {
+    public void getByName(String name, DatabaseModelListener<Pokemon> listener) {
         return null;
     }
 
     @Override
     public List<Pokemon> getAll() {
-        return null;
+        try {
+            return SQLite.select().from(Pokemon.class).queryList();
+        } catch (Exception e) {
+            Timber.e(e, "Exception occurred while fetching list of pokemons");
+            return new ArrayList<>();
+        }
     }
 
     @Override
-    public List<Pokemon> getAll(DatabaseModelListener<Pokemon> listener) {
-        return null;
+    public void getAll(final DatabaseModelListener<Pokemon> listener) {
+        SQLite.select().from(Pokemon.class).async().queryResultCallback(new QueryTransaction.QueryResultCallback<Pokemon>() {
+            @Override
+            public void onQueryResult(QueryTransaction transaction, @NonNull CursorResult<Pokemon> tResult) {
+                List<Pokemon> results = tResult.toListClose();
+
+                if(results == null || results.isEmpty()){
+                    listener.onNoModelFound();
+                }
+                else{
+                    listener.onModelLoaded();
+                }
+            }
+        }).execute();
+
     }
 }
